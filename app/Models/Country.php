e34a77c4bd3abc0a14a9e484cdd8586d2eb933d3
sivefunc <sivefunc@tuta.io>
2025-03-19 15:45:11 -0400

    feat: State table

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Country extends Model
 {
@@ -15,4 +16,9 @@ class Country extends Model
         'code',
         'phonecode'
     ];
+
+    public function states(): HasMany
+    {
+        return $this->hasMany(State::class);
+    }
 }
